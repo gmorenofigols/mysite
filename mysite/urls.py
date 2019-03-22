8601e875f06ddf6e1b80e55e16bd665d731c7702
gmorenofigols <36441200+gmorenofigols@users.noreply.github.com>
2019-03-22 10:08:28 +0100

    SignUp system included

--- mysite/urls.py
+++ mysite/urls.py
@@ -14,11 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.contrib.auth import login
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('accounts.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('', include('bookshop.urls')),
 ]
